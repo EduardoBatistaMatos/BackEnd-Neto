e8ba3c475191f4e9331b85fac1f405df983cb00d
Eduardo Batista <dudubmatos@gmail.com>
2024-08-13 21:49:52 -0300

    2

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var express = require ('express');
 var app = express();
 const port = 3000;
 
-app.listen(port, ()=> {
-    console.log('Aplicativo rodando na Porta ${port}');
+//rota do projeto
+
+app.get('/', (req, res) => {
+    console.log('Rota Raiz Encontrada!!!');
+    res.send('<h3> Rota Raiz Encontrada! <br> Olá Mundo </h3>');
+
 })
 
+//servidor rodando
+
+app.listen(port, () => {
+    console.log(`Aplicativo Rodando na Porta ${port}`);
+})   
